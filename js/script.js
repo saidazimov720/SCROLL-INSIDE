9313ef8d9793d482c1450a53a14c81463d8b67a8
saidazimov720 <saidazimovnurbek01gmail.com>
2024-05-06 15:56:22 +0500

    correction and fixing to js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
 let zSpacing = -1000,
-    lastPos = ZSpacing / 5,
+    lastPos = zSpacing / 5,
     $frames = document.getElementsByClassName('frame'),
     frames = Array.from($frames),
     zVals = []
 
-
 window.onscroll = function () {
+
     let top = document.documentElement.scrollTop,
-    delta = lastPos - top
+        delta = lastPos - top
 
     lastPos = top
 
-    frames.forEach(function(n, i) {
-		zVals.push((i * zSpacing) + zSpacing)
-		zVals[i] += delta * -5.5
-		let frame = frames[i],
-				transform = `translateZ(${zVals[i]}px)`,
-				opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
-		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
-	})
+    frames.forEach(function (n, i) {
+        zVals.push((i * zSpacing) + zSpacing)
+        zVals[i] += delta * -5.5
+        let frame = frames[i],
+            transform = `translateZ(${zVals[i]}px)`,
+            opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+        frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+    })
+
 }
 
 window.scrollTo(0, 1)
 
-let soundbutton = document.querySelector('.soundbutton'),
-audio = document.querySelector('.audio')
 
-soundbutton.addEventListener('click', e => {
-    soundbutton.classList.toggle('paused')
+let soundButton = document.querySelector('.soundbutton'),
+    audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+    soundButton.classList.toggle('paused')
     audio.paused ? audio.play() : audio.pause()
 })
 
 window.onfocus = function () {
-    soundbutton.classList.contains('paused') ? audio.pause() : audio.play() 
+    soundButton.classList.contains('paused') ? audio.pause() : audio.play()
 }
 
 window.onblur = function () {
